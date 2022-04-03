af6e81830d617dee8d7d14b1f09fa18bb512c2bb
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2022-04-03 13:39:22 -0300

    add access control in endpoints

--- BACKEND/src/routes/clients.routes.js
+++ BACKEND/src/routes/clients.routes.js
@@ -1,16 +1,29 @@
 import { Router } from 'express';
 import multer from 'multer';
-import * as clientsController from '../controllers/clients.controller';
+import * as clientsCtrl from '../controllers/clients.controller';
+import * as jwtCtrl from '../middlewares/authJwt';
 
 const upload = multer({ dest: 'uploads/' });
 const router = Router();
 
-router.get('/getAll', clientsController.getAll);
+router.get('/getAll', clientsCtrl.getAll);
 
-router.get('/getByCliente/:cliente', clientsController.getByCliente);
+router.get(
+  '/getByCliente/:cliente',
+  [jwtCtrl.verifyToken, jwtCtrl.isComer],
+  clientsCtrl.getByCliente,
+);
 
-router.get('/getByZona/:zona', clientsController.getByZona);
+router.get(
+  '/getByZona/:zona',
+  [jwtCtrl.verifyToken, jwtCtrl.isEjecutivo],
+  clientsCtrl.getByZona,
+);
 
-router.post('/newFile', upload.single('file'), clientsController.newFile);
+router.post(
+  '/newFile',
+  [upload.single('file'), jwtCtrl.verifyToken, jwtCtrl.isBackoffice],
+  clientsCtrl.newFile,
+);
 
 export default router;
